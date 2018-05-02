3995d89a4118d68c4a23b715fd8c424506bfa890
asim <asim_mave@hotmail.com>
2018-05-02 12:01:19 -0700

    view disappear added

--- SingleTest/ViewController.swift
+++ SingleTest/ViewController.swift
@@ -18,11 +18,16 @@ class ViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         //did appear
     }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
     
    
 
