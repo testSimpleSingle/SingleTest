138ea80cd8c1fd0ee014b7be1660651bfd46c6d9
asim <asim_mave@hotmail.com>
2018-05-02 11:52:25 -0700

    viewdidappear added

--- SingleTest/ViewController.swift
+++ SingleTest/ViewController.swift
@@ -14,11 +14,17 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        //did appear
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+   
 
 
 }
